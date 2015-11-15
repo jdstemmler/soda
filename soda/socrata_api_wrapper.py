1c304b6c167f50affd523cf08eac989ef9c8151d
Jayson Stemmler <jdstemmler@gmail.com>
2015-11-15 10:28:48 -0800

    added some docstrings

--- soda/socrata_api_wrapper.py
+++ soda/socrata_api_wrapper.py
@@ -10,8 +10,18 @@ class TokenError(Exception):
 
 
 class SODA(object):
+    """Socrata Open Data API Object"""
 
     def __init__(self, api_url, token=None, query=None, **kwargs):
+        """Initialize the SODA object.
+
+        INPUTS
+            api_url: (string) the url of the API endpoint.
+            token:   (string) your application token
+            query:   (dict)   dictionary containing your query
+            kwargs:  any other keyword argument for your query
+        """
+
         self.api_url = api_url
         self.url = None
         self.token = token
@@ -25,6 +35,8 @@ class SODA(object):
             self._parse_query()
 
     def set_query(self, query=None, **kwargs):
+        """Set the API query if you haven't already"""
+
         self.query = query
         if self.query is None:
             self.query = {}
@@ -32,16 +44,23 @@ class SODA(object):
         self._parse_query()
 
     def print_query(self):
+        """Print out your query"""
+
         pprint.pprint(self.query)
 
     def set_token(self, token):
+        """Set the token if you haven't already"""
+
         self.token = token
         self._parse_query()
 
-    def _parse_string(self, q):
+    def _format_string(self, q):
+        """Format the string for the web"""
+
         return urllib.quote(q, safe='=>:?$&/')
 
     def _parse_query(self):
+        """Parse the query dictionary"""
 
         if self.token is None:
             raise TokenError("Please set your token! (use .set_token(token))")
@@ -54,9 +73,11 @@ class SODA(object):
                                                   token=self.token)
         q += ''.join(querylist)
 
-        self.url = self._parse_string(q)
+        self.url = self._format_string(q)
 
     def get_request(self):
+        """Send the query to the API endpoint and get the raw data"""
+
         if self.token is None:
             raise TokenError("Please set your token! (use .set_token(token))")
 
@@ -67,6 +88,8 @@ class SODA(object):
         return request
 
     def get_df(self):
+        """Get the data as a pandas DataFrame"""
+
         if self.api_url.endswith('.json'):
             return pd.read_json(self.get_request())
         elif self.api_url.endswith('.csv'):
@@ -77,17 +100,19 @@ class SODA(object):
 
 if __name__ == "__main__":
 
-    #from settings import APP_TOKEN
+    # from settings import APP_TOKEN
 
     today = datetime.date.today()
     td = today.strftime('%Y-%m-%dT%H:%M:%S')
 
+    # token = APP_TOKEN
     token = 'abcd1234'
-    url = 'https://data.seattle.gov/resource/3k2p-39jp.json'
+    api_url = 'https://data.seattle.gov/resource/3k2p-39jp.json'
+    query = dict(limit=10,
+                 where="event_clearance_date >= '{}'".format(td)
+                 )
 
-    data = SODA(url, token=token)
-    data.set_query(limit=10,
-                   where="event_clearance_date >= '{}'".format(td))
+    data = SODA(api_url, token=token, query=query)
 
     df = data.get_df()
     print df
