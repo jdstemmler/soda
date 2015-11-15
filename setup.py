6efcd03d50124c85ed2e0895e02ab73dcc665a3f
Jayson Stemmler <jdstemmler@gmail.com>
2015-11-14 21:27:37 -0800

    added setup file for installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='soda',
+      version='0.0.1',
+      py_modules=['soda'],
+      author='Jayson Stemmler',
+      description='Socrata Open Data API Wrapper for Python',
+      )
